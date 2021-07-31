ddadd1ecd367b6253049af4baa1baa51362e4706
aaaaa <aaaaa>
2021-07-31 19:39:17 +0900

    python flaskで作成中

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-##  return "Hello, World!"
+def index():
   return render_template('index.html')
+
+@app.route("/getTickerBi")
+def getticker():
+  return {'ask' : 500.1, 'bid' : 500.2}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
